3412cccf744c0969fa504561e01c1c38dc8f837e
Phat Le <devphatle@gmail.com>
2017-08-03 04:31:45 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://phatle:123123@ds139425.mlab.com:39425/user' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
